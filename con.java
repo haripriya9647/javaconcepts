d689c608f7dd39af75b0087d00a96a2a9b82dd4f
Haripriya <arrahppk@gmail.com>
2024-02-24 21:42:22 +0530

    constructor chaining

--- con.java
+++ con.java
@@ -1,19 +1,19 @@
+// if you want to call one constructor inside another you need to make use of this() keyword. There are 2 rules, first is the method should start with this() keyword also and we also cannot use more than one this() keyword in the same method.
+
 public class con {
-    String name;
-    int age;
+    con(int parentage) {
+        int age = parentage;
+        System.out.println(age);
+    }
     
     con(String myname, int myage) {
-        this.name = myname;
-        this.age = myage;
+        this(20);
+       String name = myname;
+       int age = myage;
+        System.out.println(name + " " + age);
     }
-    
-    void display() {
-        System.out.println(name  + " " + age);
-    }
+
     public static void main(String[] args) {
         con ob = new con("hari", 22);
-        con ob1 = new con("hari", 22);
-        ob.display();
-        ob1.display();
     }
 }
